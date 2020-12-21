4313c1441d895f593838375180a7c65ce1b065dc
Gabriel-Severo <gabriel.ivi@hotmail.com>
2020-12-21 12:12:06 -0400

    Produtos adicionados pela api

--- frontend/app.js
+++ frontend/app.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:1337/"
+const BASE_URL = "http://localhost:1337"
 
 async function carregarDados(colecao) {
     const resposta = await fetch(`${BASE_URL}${colecao}`)
@@ -7,20 +7,41 @@ async function carregarDados(colecao) {
 }
 
 async function buscarProdutos() {
-    const produtos = await carregarDados('products')
-    console.log(produtos)
+    const produtos = await carregarDados('/products')
+    return produtos
 }
 
 async function buscarTamanhos() {
-    const produtos = await carregarDados('sizes')
-    console.log(produtos)
+    const tamanhos = await carregarDados('/sizes')
+    return tamanhos
 }
 
 async function buscarEstilos() {
-    const produtos = await carregarDados('styles')
-    console.log(produtos)
+    const estilos = await carregarDados('/styles')
+    return estilos
 }
 
-buscarTamanhos()
-buscarEstilos()
-buscarProdutos()
+async function adicionarProdutosAODOM() {
+    const produtos = await buscarProdutos()
+    for (const produto of produtos) {
+        let {name, regular_price, image} = produto
+        regular_price = regular_price.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+        document.querySelector('.products').innerHTML += `
+            <div class="product">
+                <div class="product-image">
+                    <img src="${BASE_URL}${image[0].url}" alt="Produto">
+                </div>
+                <div class="product-description">
+                    <div class="product-name">
+                        <p>${name}</p>
+                    </div>
+                    <div class="product-price">
+                        <p>${regular_price}</p>
+                    </div>
+                </div>
+            </div>
+        `
+    }
+}
+
+adicionarProdutosAODOM()
